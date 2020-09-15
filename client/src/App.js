f3acc467fb6982a6f10620fc50c7a5f5ca3c1685
FlorianSahbi <florian.sahbi@gmail.com>
2020-09-16 00:31:10 +0200

    add lock button

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,33 @@ function App() {
   const { loading, error, data: dataOldMarkers } = useQuery(GET_MARKERS);
   const { data: dataNewMarker } = useSubscription(MARKER_ADDED);
   const [zoom, setZoom] = useState(20);
+  const [lock, setLock] = useState(false);
+
+  function LockButton() {
+    return (
+      <div
+        onClick={() => setLock(!lock)}
+        style={{
+          position: "absolute",
+          height: "50px",
+          width: "50px",
+          border: "2px solid black",
+          zIndex: 999,
+          bottom: 0,
+          borderRadius: "50%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          margin: "1rem",
+          cursor: "pointer",
+          backgroundColor: "rgba(0,0,0,0.5)",
+          color: "white",
+        }}
+      >
+        <p>Lock</p>
+      </div>
+    )
+  }
 
   if (loading) return <div>Loading...</div>;
 
@@ -24,20 +51,22 @@ function App() {
   if (dataOldMarkers) {
     const currentLocation = getCurrentLocation(dataOldMarkers, dataNewMarker);
     return (
-      <Map
-        center={[48.866667, 2.333333]}
-        zoom={zoom}
-      >
-        {console.log(currentLocation)}
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <Marker
-          key={currentLocation._id}
-          position={[currentLocation.latitude, currentLocation.longitude]}
-        />
-      </Map>
+      <>
+        <LockButton />
+        <Map
+          center={lock ? [currentLocation.latitude, currentLocation.longitude] : [48.866667, 2.333333]}
+          zoom={zoom}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker
+            key={currentLocation._id}
+            position={[currentLocation.latitude, currentLocation.longitude]}
+          />
+        </Map>
+      </>
     );
   }
   return <></>;
